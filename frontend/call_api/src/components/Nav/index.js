f87ddd487790252950a7c6edbf52ec270229e99d
16t1021064 <16t1021064@husc.edu.vn>
2021-07-30 17:22:08 +0700

    dang sua giao dien

--- frontend/call_api/src/components/Nav/index.js
+++ frontend/call_api/src/components/Nav/index.js
@@ -21,13 +21,6 @@ export default class Nav extends Component {
                                 </ul>
                             </li>
                         </ul>
-                        <form className="d-flex">
-                            <button className="btn btn-outline-dark" type="submit">
-                                <i className="bi-cart-fill me-1" />
-                                Cart
-                                <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                            </button>
-                        </form>
                     </div>
                 </div>
             </nav>
