e4617a2d1b67a98371183a2d0206a274309263c8
16t1021064 <16t1021064@husc.edu.vn>
2021-08-01 18:58:15 +0700

    da chinh xong giao dien

--- frontend/call_api/src/components/AdminHomePage/index.js
+++ frontend/call_api/src/components/AdminHomePage/index.js
@@ -4,7 +4,7 @@ export default class AdminHomePage extends Component {
     render() {
         return (
             <div>
-                <img className="img" src="https://vietadsgroup.vn/Uploads/files/trangchu-la-gi.png" alt="ảnh trang chủ"></img>
+                <img className="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfGsyB7CcE25bzKCaYe5MrS_tixL0SFcRHDg&usqp=CAU" alt="ảnh trang chủ"></img>
             </div>
         )
     }
